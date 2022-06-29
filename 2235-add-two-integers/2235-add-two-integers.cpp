27c9f6898c21c636219b330a4dfc4d7f4f67849c
SyedDaniyalHassan <57272783+SyedDaniyalHassan@users.noreply.github.com>
2022-06-29 17:02:05 +0500

    Time: 4 ms (26.08%), Space: 5.9 MB (23.60%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -3,7 +3,7 @@ public:
     int sum(int num1, int num2) {
         int sum =0;
         sum =num1+num2;
-        for (int i = 0; i< 1000; i++);
+        for (int i = 0; i< 1000000; i++);
         return(sum);
     }
 };
